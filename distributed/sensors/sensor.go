70f5ff4f2d90fbf22b0d207480995a0e2b53dc05
Ramandeep Kaur <rdeepk@gmail.com>
2016-11-02 12:36:45 -0700

    sensor flags added

---
+++ distributed/sensors/sensor.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"flag"
+)
+
+var name = flag.String("name", "sensor", "name of the sensor")
+var freq = flag.Uint("freq", 5, "update frequency in  cycles/sec")
+var max = flag.Float64("max", 5., "maximum value for generated readings")
+var min = flag.Float64("min", 1., "minimum value for generated readings")
+var stepsize = flag.Float64("stepsize", 0.1, "maximum allowable change per measurement")
+
+func main() {
+	flag.Parse()
+
+}
